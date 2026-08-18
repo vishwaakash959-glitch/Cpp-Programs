6d39e62646e525a8d4c3b0e95560d63839ef6708
VISHWA <vishwaakash959@gmail.com>
2026-08-18 20:45:22 +0530

    Initilize Repository

---
+++ Singly Linked List.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+// singly linked list node structure
+class Node {
+public:
+    int data;
+    Node* next;
+
+    // constructor to initialize a new node with data
+    Node(int new_data) {
+        this->data = new_data;
+        this->next = nullptr;
+    }
+};
+
+int main() {
+    
+    // Create the first node (head of the list)
+    Node* head = new Node(10);
+
+    // Link the second node
+    head->next = new Node(20);
+
+    // Link the third node
+    head->next->next = new Node(30);
+
+    // Link the fourth node
+    head->next->next->next = new Node(40);
+
+    // printing linked list
+    Node* temp = head;
+    while (temp != nullptr) {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+}
